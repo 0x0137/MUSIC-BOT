877ad0fdce8dc918670b7e106f6d580aefc11fdd
0x0137 <82236830+0x0137@users.noreply.github.com>
2024-01-28 13:46:14 +0300

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,8 +22,8 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v3.5`, 
-  type: ActivityType.Watching }), 10000);
+  name: `تدريس المرجلة`, 
+  type: ActivityType.Playing }), 10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
